b07752f2abdfdd4c849284129e52543395a7d4f8
Bunchhoung Tiv <38189366+chhoungt@users.noreply.github.com>
2019-03-26 16:08:16 -0400

    Quickster V1.3
    Autofill address early implementation

--- src/ButtonToggle.js
+++ src/ButtonToggle.js
@@ -3,6 +3,7 @@ import './ButtonToggle.css';
 import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import { Input } from 'reactstrap';
 import { Container, Segment} from "semantic-ui-react";
+import PlacesAutocomplete, { geocodeByAddress, getLatLng, } from 'react-places-autocomplete';
 
 export class ButtonToggle extends Component {
     constructor(props) {
@@ -100,12 +101,74 @@ export class ButtonToggle extends Component {
                    Array(this.state.curre).fill(0).map( (_,x) => <Input key={x} placeholder="Address"></Input>)
                }  */}
         </div>
-        
+
         <button onClick={checker}>
         Submit
         </button>
       </div>
     )
+  }
+}
+
+//Source: https://github.com/hibiken/react-places-autocomplete?fbclid=IwAR03LOgSodZOcy2mxnpt7XjGlGT0cc7w5HXd-eNeAOy25Oi3sgyvSL9R-3Q
+class LocationSearchInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { address: '' };
+  }
+
+  handleChange = address => {
+    this.setState({ address });
+  };
+
+  handleSelect = address => {
+    geocodeByAddress(address)
+      .then(results => getLatLng(results[0]))
+      .then(latLng => console.log('Success', latLng))
+      .catch(error => console.error('Error', error));
+  };
+
+  render() {
+    return (
+      <PlacesAutocomplete
+        value={this.state.address}
+        onChange={this.handleChange}
+        onSelect={this.handleSelect}
+      >
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+          <div>
+            <input
+              {...getInputProps({
+                placeholder: 'Search Places ...',
+                className: 'location-search-input',
+              })}
+            />
+            <div className="autocomplete-dropdown-container">
+              {loading && <div>Loading...</div>}
+              {suggestions.map(suggestion => {
+                const className = suggestion.active
+                  ? 'suggestion-item--active'
+                  : 'suggestion-item';
+                // inline style for demonstration purpose
+                const style = suggestion.active
+                  ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                  : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                return (
+                  <div
+                    {...getSuggestionItemProps(suggestion, {
+                      className,
+                      style,
+                    })}
+                  >
+                    <span>{suggestion.description}</span>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </PlacesAutocomplete>
+    );
   }
 }
 
