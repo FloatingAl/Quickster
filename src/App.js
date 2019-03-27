87b60b5c96a484a7c6f2d0da8cf1c42b3b427290
AlbaraMehene <Albara_Mehene@student.uml.edu>
2019-03-27 17:06:30 -0400

    Added Button Toggle & Map Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 import { ButtonToggle } from './ButtonToggle'
+import { MapContainer } from './Map.js'
+import { apiKey } from'./google_api_key.js'
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 //import { InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
 
 class App extends Component {
@@ -10,9 +13,14 @@ class App extends Component {
         <header className="App-header">
         <ButtonToggle message = "Choose..."/>
         </header>
+        {/* <MapContainer/> */}
       </div>
     );
   }
 }
 
 export default App;
+
+// export default GoogleApiWrapper({
+//   apiKey: (apiKey)
+// })(MapContainer)
--- src/ButtonToggle.js
+++ src/ButtonToggle.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 import './ButtonToggle.css';
 import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
-import { Input } from 'reactstrap';
+//import { Input } from 'reactstrap';
 //import { Container, Segment} from "semantic-ui-react";
-import PlacesAutocomplete, { geocodeByAddress, getLatLng, } from 'react-places-autocomplete';
+//import PlacesAutocomplete, { geocodeByAddress, getLatLng, } from 'react-places-autocomplete';
+import { GoogleComponent } from 'react-google-location' 
+
+const apikey = "AIzaSyCkF2kq7ElyH4OtWelckXaKktYWqIi86dU";
 
 export class ButtonToggle extends Component {
     constructor(props) {
@@ -19,6 +22,9 @@ export class ButtonToggle extends Component {
           on2: false,
           on3: false,
           on4: false,
+          start: null,
+          first: null,
+          second: null,
         };
       }
     
@@ -47,6 +53,8 @@ export class ButtonToggle extends Component {
             on4: false,
             currentClick: 2,
         });
+
+        
     }
 
     toggleButton3 = () => {
@@ -75,7 +83,16 @@ export class ButtonToggle extends Component {
     return (
       <div>
         <h2>Please enter a starting address</h2>
-        <Input className= "input" id = "inputStart" placeholder="Starting Address"></Input>
+        {/* <Input className= "input" id = "inputStart" placeholder="Starting Address"></Input> */}
+        <GoogleComponent
+            id = "inputStart"
+            apiKey={apikey}
+            language={'en'}
+            country={'country:in|country:us'}
+            coordinates={true}
+            locationBoxStyle={'input'}
+            locationListStyle={'slide'}
+            onChange={(e) => { this.setState({ start: e }) }} />
         <p>Please choose the amount of address:</p>
         
         <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
@@ -93,16 +110,28 @@ export class ButtonToggle extends Component {
         </DropdownMenu>
       </ButtonDropdown>
          <div className ="inputG">
-             {this.state.on && Array(this.state.currentClick).fill(0).map((_,x) => <Input onChange="" className= "inputA" id = "input1" key = {x} placeholder="Address"></Input>)}
-             {this.state.on2 && Array(this.state.currentClick).fill(0).map((_,x) => <Input className= "inputA" id = "input2" key = {x} placeholder="Address"></Input>)}
-             {this.state.on3 && Array(this.state.currentClick).fill(0).map((_,x) => <Input className= "inputA" id = "input3" key = {x} placeholder="Address"></Input>)}
-             {this.state.on4 && Array(this.state.currentClick).fill(0).map((_,x) => <Input className= "inputA" id = "input4" key = {x} placeholder="Address"></Input>)}
-               {/* {
-                   Array(this.state.curre).fill(0).map( (_,x) => <Input key={x} placeholder="Address"></Input>)
-               }  */}
+             {this.state.on && Array(this.state.currentClick).fill(0).map((_,x) =><GoogleComponent 
+             apiKey={apikey} 
+             id = "input1" language={'en'} country={'country:in|country:us'} coordinates={true} locationBoxStyle={'inputA'}locationListStyle={'slide'}
+             onChange={(e) => { this.setState({ first: e }) }} /> )}
+            
+            { this.state.on2 && Array(this.state.currentClick).fill(0).map((_,x) =><GoogleComponent 
+             apiKey={apikey} 
+             id = "input2" language={'en'} country={'country:in|country:us'} coordinates={true} locationBoxStyle={'inputA'}locationListStyle={'slide'}
+             onChange={(e) => { this.setState({ first: e  }) }} /> )}
+
+            {this.state.on3 && Array(this.state.currentClick).fill(0).map((_,x) =><GoogleComponent 
+             apiKey={apikey} 
+             id = "input3" language={'en'} country={'country:in|country:us'} coordinates={true} locationBoxStyle={'inputA'}locationListStyle={'slide'}
+             onChange={(e) => { this.setState({ first: e }) }} /> )}
+
+            {this.state.on4 && Array(this.state.currentClick).fill(0).map((_,x) =><GoogleComponent 
+             apiKey={apikey} 
+             id = "input4" language={'en'} country={'country:in|country:us'} coordinates={true} locationBoxStyle={'inputA'}locationListStyle={'slide'}
+             onChange={(e) => { this.setState({ first: e }) }} /> )}
         </div>
 
-        <button type="submit" onClick={checker}>
+        <button type="submit">
         Submit
         </button>
       </div>
@@ -110,67 +139,67 @@ export class ButtonToggle extends Component {
   }
 } 
 
-//Source: https://github.com/hibiken/react-places-autocomplete?fbclid=IwAR03LOgSodZOcy2mxnpt7XjGlGT0cc7w5HXd-eNeAOy25Oi3sgyvSL9R-3Q
-class LocationSearchInput extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { address: '' };
-  }
+// //Source: https://github.com/hibiken/react-places-autocomplete?fbclid=IwAR03LOgSodZOcy2mxnpt7XjGlGT0cc7w5HXd-eNeAOy25Oi3sgyvSL9R-3Q
+// class LocationSearchInput extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { address: '' };
+//   }
 
-  handleChange = address => {
-    this.setState({ address });
-  };
+//   handleChange = address => {
+//     this.setState({ address });
+//   };
 
-  handleSelect = address => {
-    geocodeByAddress(address)
-      .then(results => getLatLng(results[0]))
-      .then(latLng => console.log('Success', latLng))
-      .catch(error => console.error('Error', error));
-  };
+//   handleSelect = address => {
+//     geocodeByAddress(address)
+//       .then(results => getLatLng(results[0]))
+//       .then(latLng => console.log('Success', latLng))
+//       .catch(error => console.error('Error', error));
+//   };
 
-  render() {
-    return (
-      <PlacesAutocomplete
-        value={this.state.address}
-        onChange={this.handleChange}
-        onSelect={this.handleSelect}
-      >
-        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-          <div>
-            <input
-              {...getInputProps({
-                placeholder: 'Search Places ...',
-                className: 'location-search-input',
-              })}
-            />
-            <div className="autocomplete-dropdown-container">
-              {loading && <div>Loading...</div>}
-              {suggestions.map(suggestion => {
-                const className = suggestion.active
-                  ? 'suggestion-item--active'
-                  : 'suggestion-item';
-                // inline style for demonstration purpose
-                const style = suggestion.active
-                  ? { backgroundColor: '#fafafa', cursor: 'pointer' }
-                  : { backgroundColor: '#ffffff', cursor: 'pointer' };
-                return (
-                  <div
-                    {...getSuggestionItemProps(suggestion, {
-                      className,
-                      style,
-                    })}
-                  >
-                    <span>{suggestion.description}</span>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        )}
-      </PlacesAutocomplete>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <PlacesAutocomplete
+//         value={this.state.address}
+//         onChange={this.handleChange}
+//         onSelect={this.handleSelect}
+//       >
+//         {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+//           <div>
+//             <input
+//               {...getInputProps({
+//                 placeholder: 'Search Places ...',
+//                 className: 'location-search-input',
+//               })}
+//             />
+//             <div className="autocomplete-dropdown-container">
+//               {loading && <div>Loading...</div>}
+//               {suggestions.map(suggestion => {
+//                 const className = suggestion.active
+//                   ? 'suggestion-item--active'
+//                   : 'suggestion-item';
+//                 // inline style for demonstration purpose
+//                 const style = suggestion.active
+//                   ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+//                   : { backgroundColor: '#ffffff', cursor: 'pointer' };
+//                 return (
+//                   <div
+//                     {...getSuggestionItemProps(suggestion, {
+//                       className,
+//                       style,
+//                     })}
+//                   >
+//                     <span>{suggestion.description}</span>
+//                   </div>
+//                 );
+//               })}
+//             </div>
+//           </div>
+//         )}
+//       </PlacesAutocomplete>
+//     );
+//   }
+// }
 
 function checker() {
 
