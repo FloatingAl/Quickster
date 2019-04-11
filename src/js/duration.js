0cfe4558d1e2518a2aadc68367899d884703a01b
Liam <liam_reynolds@student.uml.edu>
2019-04-11 16:25:51 -0400

    test

--- src/js/duration.js
+++ src/js/duration.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-export function getDuration(origin, destination, apikey) {
-    var origin = 'Lowell,Massachusetts';
-    var destination = 'Boston,Massachusetts';
-=======
 export function getDuration(apikey, A, B) {
     var origin = A;
     var destination = B;
